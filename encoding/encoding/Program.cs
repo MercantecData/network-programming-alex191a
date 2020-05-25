dfc4274416f0cdd837f8dc5ebc0a6430d7ca9a32
alexander olsen <alex191a@gmail.com>
2020-05-25 09:27:40 +0200

    update with progress

--- encoding/encoding/Program.cs
+++ encoding/encoding/Program.cs
@@ -10,6 +10,16 @@ namespace encoding
     {
         static void Main(string[] args)
         {
+            string text = Console.ReadLine();
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            foreach (byte Integer in bytes)
+            {
+                Console.WriteLine(Integer);
+            }
+            string shit = Encoding.UTF8.GetString(bytes);
+            Console.WriteLine(shit);
+            Console.ReadKey();
+
         }
     }
 }
