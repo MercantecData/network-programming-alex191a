554fcc77624985e100b8c1966a526609af3ae5d0
alexander olsen <alex191a@gmail.com>
2020-05-26 09:24:48 +0200

    experimental client server
    Denne er clunky men virker overall

--- encoding/encoding/Program.cs
+++ encoding/encoding/Program.cs
@@ -44,6 +44,43 @@ namespace encoding
                 stream.Write(buffersize, 0, message.Length);
                 client.Close();
             }
+            void ClientServer()
+            {
+                bool bol = true;
+                IPAddress ip = IPAddress.Any;
+                IPEndPoint endpoint = new IPEndPoint(ip, 5001);
+
+                TcpListener listener = new TcpListener(endpoint);
+                listener.Start();
+                while (bol)
+                {
+                    if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+                    {
+                        Console.WriteLine("skriv den ip du vil sende til\n example: 127.0.0.1");
+                        string ipInput = Console.ReadLine();
+                        TcpClient clients = new TcpClient();
+                        int port = 5001;
+                        IPAddress ipp = IPAddress.Parse(ipInput);
+                        IPEndPoint remoteEndpoint = new IPEndPoint(ipp, port);
+                        clients.Connect(remoteEndpoint);
+                        NetworkStream streams = clients.GetStream();
+                        string message = Console.ReadLine();
+                        byte[] buffersixe = Encoding.UTF8.GetBytes(message);
+
+                        streams.Write(buffersixe, 0, message.Length);
+                        clients.Close();
+                    }
+                    TcpClient client = listener.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+                        byte[] buffersize = new byte[1000];
+                        int nbytesRead = stream.Read(buffersize, 0, 1000);
+                        string messageRecieve = Encoding.UTF8.GetString(buffersize, 0, nbytesRead);
+                        Console.WriteLine(messageRecieve);
+                }
+                listener.Stop();
+                
+
+            }
             bool die = true;
             while (die)
             {
@@ -61,7 +98,8 @@ namespace encoding
                     }
                     else if (Convert.ToInt32(reader) == 3)
                     {
-
+                        Console.WriteLine("press enter to write a message. press everything else to load if you got a message");
+                        ClientServer();
                     }
                 }
                 if (reader == "exit")
